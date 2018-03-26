5f3943d3cf31afab73db5b6634bb4563839bef41
Fredrik Olofsson <evoozer@gmail.com>
2018-03-26 07:56:17 +0200

    client/serve - Fixed ssl/ca

--- src/client/factory/serve/serve.mjs
+++ src/client/factory/serve/serve.mjs
@@ -29,7 +29,7 @@ export default [
 
       const key = ssl.key ? `${clientDirectory}/${ssl.key}` : '';
       const cert = ssl.cert ? `${clientDirectory}/${ssl.cert}` : '';
-      const ca = ssl.cert ? `${clientDirectory}/${ssl.ca}` : '';
+      const ca = ssl.ca ? `${clientDirectory}/${ssl.ca}` : '';
 
       const content =`import Module from 'evoozer/Module';
 import WebApplication from 'evoozer/Module/web-application';
@@ -45,7 +45,7 @@ const app = new Module(null, [ WebApplication, src ])
     const caPath = ${JSON.stringify(ca)};
     const key = keyPath ? fs.readFileSync(keyPath) : null;
     const cert = certPath ? fs.readFileSync(certPath) : null;
-    const ca = caPath ? fs.readFileSync(caPath) : null;
+    const ca = caPath ? [fs.readFileSync(caPath)] : null;
     console.log('webApplicationProvider port: ', port);
     webApplicationProvider
       ${publicDirectories.map(toAddPublicDirectoryCall).join('\n')}
